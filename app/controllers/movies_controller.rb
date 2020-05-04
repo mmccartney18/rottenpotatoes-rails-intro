fafab7e945e44157d648513f517f2d9de24bdddd
mmccartney18 <mmccartney18@punahou.edu>
2020-05-04 18:34:34 +0000

    Final Version

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,98 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+       #Part 3:
+   #for each of these two options (sort_by. ratings)
+   #1. if the params[]hash gives explicit values for those override them in session
+   #2. otherwise if they are stored preferences in the sessions. use those to fill in params
+   #3 if neither params or nor sessions are availible, apply defaults
+
+   params.permit!
+    @all_ratings = Movie.all_ratings
+    #makes check boxes appear if appeared (clicked) true
+    @ratings = params[:ratings]
+    #part 3 
+    #redirect is like refresh
+    redirect = false
+    
+
+    if params[:sort_by]
+        @sort_by = params[:sort_by]
+        session[:sort_by] = params[:sort_by]
+    elsif session[:sort_by]
+        @sort_by = session[:sort_by]
+        redirect = true
+    else
+        @sort_by = nil
+    end
+
+    if params[:commit] == "Refresh" and params[:ratings].nil?
+        @ratings = nil
+        session[:ratings] = nil
+    elsif params[:ratings]
+        @ratings = params[:ratings]
+        session[:ratings] = params[:ratings]
+    elsif session[:ratings]
+        @ratings = session[:ratings]
+        redirect = true
+    else 
+        @ratings = nil
+    end
+
+    if redirect
+        flash.keep
+        redirect_to movies_path :sort_by=>@sort_by, :ratings=>@ratings
+    end
+    
+    #@movies = Movie.all
+    #@sort_by = params[:sort_by]
+    #@ratings = params[:ratings]
+    #params.permit!
+    if @ratings and @sort_by
+        #@movies = Movie.where(:rating => params[:ratings].keys).order[:sort_by]
+       # @movies = Movie.where(:rating => params[:ratings].keys).order(params[:sort_by])
+        @movies = Movie.where(:rating => @ratings.keys).order(params[:sort_by])
+        #@movies = Movie.where(:rating => params[:ratings].keys).find(:all, :order =>(params[:sort_by])
+        #@movies = Movie.where(:rating => params[:ratings].keys).find(:all, :order(sort_by)
+    elsif @ratings 
+        #@movies = Movie.where(:rating => params[:ratings].keys)
+        @movies = Movie.where(:rating => @ratings.keys)
+    elsif @sort_by
+        @movies = @movies.order(@sort_by)
+    else
+        @movies = Movie.all
+    end 
+    
+    if !@ratings
+        @ratings = Hash.new
+    end
+
+   # @movies = Movie.all
+    #if params[:ratings]
+    #    @movies = Movie.where(:rating => params[:ratings].keys).order(params[:sort_by])
+    #end
+   # @sort_by = params[:sort_by]
+   # @all_ratings = Movie.all_ratings
+   # @set_ratings = params[:ratings]
+   # if !@set_ratings
+    #    @set_ratings = Hash.new
+   # end
+
+    #@sort_by = params[:sort_by]
+    #@sort_column = params[:sort_by]
+    #@movies = @movies.order(@sort_column)
+    #@all_ratings = Movie.all_ratings
+    
+    #@current_ratings = params[:ratings]
+    #if !params = [:ratings].nil?
+        #@movies = Movie.where(rating: @current_ratings.keys)
+    #end
+
+    #@set_ratings = params[:ratings]
+   #if !@set_ratings
+    #@set_ratings = Hash.new
+   #end
+
   end
 
   def new
@@ -43,3 +134,4 @@ class MoviesController < ApplicationController
   end
 
 end
+
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,10 @@
 class Movie < ActiveRecord::Base
+    #attr_accessor :title, :rating, :description, :release_date
+    def self.all_ratings
+        a = Array.new
+        self.select("rating").uniq.each {|x| a.push(x.rating)}
+        a.sort.uniq
+    
+       ['G', 'PG', 'PG-13', 'R']
+    end
 end
